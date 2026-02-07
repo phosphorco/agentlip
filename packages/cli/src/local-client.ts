7a7d721dc1599d6740b12e87e602a0aa27678f70
Cole Lawrence <cole@colelawrence.com>
2026-02-06 22:38:11 -0300

    cli: add agentlip/local-client export

---
+++ packages/cli/src/local-client.ts
@@ -0,0 +1,7 @@
+/**
+ * agentlip/local-client - Node-friendly local client entrypoint
+ *
+ * Re-export of @agentlip/client/local for users who install the CLI package.
+ */
+
+export * from "@agentlip/client/local";

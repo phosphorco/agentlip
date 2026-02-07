26353d1c28a2c896df415bbb625a2a5499eee95d
Cole Lawrence <cole@colelawrence.com>
2026-02-07 01:04:15 -0300

    kernel: fix FTS triggers for external content table

--- packages/kernel/migrations/0001_schema_v1_fts.sql
+++ packages/kernel/migrations/0001_schema_v1_fts.sql
@@ -11,22 +11,31 @@ CREATE VIRTUAL TABLE IF NOT EXISTS messages_fts USING fts5(
   content_rowid=rowid
 );
 
+-- Triggers: sync FTS with content table
+-- Note: for external content FTS5 tables, updates/deletes must be performed via
+-- special "delete" commands rather than UPDATE/DELETE statements.
+
+DROP TRIGGER IF EXISTS messages_fts_insert;
+DROP TRIGGER IF EXISTS messages_fts_update;
+DROP TRIGGER IF EXISTS messages_fts_delete;
+
 -- Trigger: sync FTS on insert
-CREATE TRIGGER IF NOT EXISTS messages_fts_insert AFTER INSERT ON messages
+CREATE TRIGGER messages_fts_insert AFTER INSERT ON messages
 BEGIN
   INSERT INTO messages_fts(rowid, content_raw) VALUES (new.rowid, new.content_raw);
 END;
 
 -- Trigger: sync FTS on update
-CREATE TRIGGER IF NOT EXISTS messages_fts_update AFTER UPDATE ON messages
+CREATE TRIGGER messages_fts_update AFTER UPDATE ON messages
 BEGIN
-  UPDATE messages_fts SET content_raw = new.content_raw WHERE rowid = old.rowid;
+  INSERT INTO messages_fts(messages_fts, rowid, content_raw) VALUES ('delete', old.rowid, old.content_raw);
+  INSERT INTO messages_fts(rowid, content_raw) VALUES (new.rowid, new.content_raw);
 END;
 
 -- Trigger: sync FTS on delete
-CREATE TRIGGER IF NOT EXISTS messages_fts_delete AFTER DELETE ON messages
+CREATE TRIGGER messages_fts_delete AFTER DELETE ON messages
 BEGIN
-  DELETE FROM messages_fts WHERE rowid = old.rowid;
+  INSERT INTO messages_fts(messages_fts, rowid, content_raw) VALUES ('delete', old.rowid, old.content_raw);
 END;
 
 COMMIT;
